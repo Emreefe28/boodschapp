ef618f872359286f629c8fbf94715a83a98b2f3a
Emre <Emre_efe1@live.nl>
2017-02-07 21:25:00 +0100

    products addable

--- Boodschapp/pages/Products.aspx.cs
+++ Boodschapp/pages/Products.aspx.cs
@@ -11,7 +11,32 @@ namespace Boodschapp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(Session["user_id"] == null)
+            {
+                Response.Redirect("login.aspx");
+            }
+        }
 
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var context = new BoodschappContext())
+            {
+                try
+                {
+                    var aankoop = new Aankoop();
+                    aankoop.product_name = txtName.Text;
+                    aankoop.price = txtPrice.Text;
+                    aankoop.User_id = Convert.ToInt32(Session["user_id"]);
+                    aankoop.created_at = String.Format("{0}", DateTime.Now);
+                    aankoop.updated_at = String.Format("{0}", DateTime.Now);
+                    context.Aankoops.Add(aankoop);
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
